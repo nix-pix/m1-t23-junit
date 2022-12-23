be7cf8ef623209d4597ec95148ddfa11812e704c
nix-pix <nix-pix@yandex.ru>
2022-12-23 23:30:58 +0300

    Дописал BankAccountTest из account

--- test/account/BankAccountTest.java
+++ test/account/BankAccountTest.java
@@ -1,9 +1,9 @@
 package account;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BankAccountTest {
 
@@ -19,5 +19,31 @@ public class BankAccountTest {
         account.activate("RUB");
         assertEquals(0, account.getAmount());
         assertEquals("RUB", account.getCurrency());
+    }
+
+    @Test
+    public void shouldBeBlockedAfterBlockIsCalled() {
+        BankAccount account = new BankAccount("a", "b");
+        account.block();
+        assertTrue(account.isBlocked());
+    }
+
+    @Test
+    public void shouldReturnFirstNameThenSecondName() {
+        BankAccount account = new BankAccount("testFirstName", "testLastName");
+        assertArrayEquals(new String[]{"testFirstName", "testLastName"}, account.getFullName());
+    }
+
+    @Test
+    public void shouldReturnNullAmountWhenNotActive() {
+        BankAccount account = new BankAccount("a", "b");
+        IllegalStateException exception = assertThrows(IllegalStateException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                account.getAmount();
+            }
+        });
+        assertNull(account.getCurrency());
+        assertEquals("Счёт не активирован.", exception.getMessage());
     }
 }
